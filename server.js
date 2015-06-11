e00ce61097fafe7f4d017a5da1bca7b00bc51851
jseiden <jseiden@gmail.com>
2015-06-10 19:05:14 -0700

    Removed ip variable

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var app = require('./server-config.js');
 
 var port = env.process.port || 4568;
-var IP = env.process.IP || 'https://jakeliamshortly.azurewebsites.net/';
+// var IP = env.process.IP || 'https://jakeliamshortly.azurewebsites.net/';
 
-app.listen(port,IP);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
